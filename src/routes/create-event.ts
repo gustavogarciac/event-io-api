57677bf9818bd233bb82a64affb81de8330a4ccf
gustavo <gustavogarciaci1@gmail.com>
2024-04-02 18:18:49 -0300

    feat: zod validation and created route to create event

---
+++ src/routes/create-event.ts
@@ -0,0 +1,60 @@
+import { ZodTypeProvider } from "fastify-type-provider-zod"
+import { generateSlug } from "../utils/generate-slug"
+import { prisma } from "../lib/prisma"
+import { z } from "zod"
+import { FastifyInstance } from "fastify"
+
+export async function createEvent(app: FastifyInstance) {
+  app
+  .withTypeProvider<ZodTypeProvider>()
+  .post("/events", {
+    schema: {
+      body: z.object({
+        title: z.string().min(4),
+        details: z.string().nullable(),
+        maximumAttendees: z.number().int().positive().nullable(),
+      }),
+      response: {
+        201: z.object({
+          event: z.object({
+            id: z.string(),
+            title: z.string(),
+            details: z.string().nullable(),
+            maximumAttendees: z.number().int().positive().nullable(),
+            slug: z.string(),
+          }),
+        })
+      }
+    }
+  }, async (req, reply) => {
+
+  const {
+    title,
+    details,
+    maximumAttendees,
+  } = req.body
+
+  const slug = generateSlug(title)
+
+  const eventWithSameSlug = await prisma.event.findFirst({
+    where: {
+      slug
+    }
+  })
+
+  if (eventWithSameSlug !== null) {
+    throw new Error("Another event with same title already exists.")
+  }
+
+  const event = await prisma.event.create({
+    data: {
+      title,
+      details,
+      maximumAttendees,
+      slug
+    }
+  })
+
+  return reply.status(201).send({event})
+})
+}
--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,13 @@
-import { PrismaClient } from "@prisma/client"
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod"
 import fastify from "fastify"
-import { z } from "zod"
+import { createEvent } from "./routes/create-event"
 
 const app = fastify()
 
-const prisma = new PrismaClient({
-  log: ["query"],
-})
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
-app.post("/events", async (req, reply) => {
-  const createEventSchema = z.object({
-    title: z.string().min(4),
-    details: z.string().nullable(),
-    maximumAttendees: z.number().int().positive().nullable(),
-  })
-
-  const {
-    title,
-    details,
-    maximumAttendees,
-  } = createEventSchema.parse(req.body)
-
-  const event = await prisma.event.create({
-    data: {
-      title,
-      details,
-      maximumAttendees,
-      slug: new Date().toISOString(), 
-    }
-  })
-
-  return reply.status(201).send({event})
-})
+app.register(createEvent)
 
 app.listen({
   port: 3333,
