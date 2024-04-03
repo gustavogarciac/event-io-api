d71b3eaa491145cb48f508cf3021a1c8b7fc2451
gustavo <gustavogarciaci1@gmail.com>
2024-04-03 16:27:23 -0300

    feat: cors

--- src/server.ts
+++ src/server.ts
@@ -11,8 +11,13 @@ import { getAttendeeBadge } from "./routes/get-attendee-badge"
 import { checkIn } from "./routes/check-in"
 import { getEventAttendees } from "./routes/get-event-attendees"
 import { errorHandler } from "./error-handler"
+import fastifyCors from "@fastify/cors"
 
 const app = fastify()
+
+app.register(fastifyCors, {
+  origin: "*",
+})
 
 app.register(fastifySwagger, {
   swagger: {
@@ -45,4 +50,5 @@ app.setErrorHandler(errorHandler)
 
 app.listen({
   port: 3333,
+  host: "0.0.0.0"
 }).then(() => console.log("🚀 HTTP Server Running"))
